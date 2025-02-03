8b28be3f5871fc272f2d7270fa33e2d29124506b
jimmy057 <jimmypolanco2540@gmail.com>
2025-02-03 19:18:24 -0400

    Modelo creado

---
+++ Models/Registro.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace JimmyConcepcion_P1_AP1.Models
+{
+    public class Registro
+    {
+        [Key]
+        public int id { get; set; }
+    }
+}
